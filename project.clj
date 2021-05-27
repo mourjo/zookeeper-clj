00562accd0681303d7a06bd985b19c6cad67446a
Mourjo Sen <mourjo.sen@gojek.com>
2021-05-27 09:39:01 +0530

    Move out test scope dependencies to dev profile

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "A Clojure DSL for Apache ZooKeeper"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.apache.zookeeper/zookeeper "3.6.1"]
-                 [commons-codec "1.14"]
-                 [org.apache.curator/curator-test "5.1.0" :scope "test"]]
+                 [commons-codec "1.14"]]
   :repositories {"clojars" {:sign-releases false :url "https://clojars.org/repo/"}}
   :global-vars {*warn-on-reflection* true}
+  :profiles {:dev {:dependencies [[org.apache.curator/curator-test "5.1.0"]]}}
   :plugins [[lein-ancient "0.6.15"]
             [lein-cljfmt "0.6.8"]])
